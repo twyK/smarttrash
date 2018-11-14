b186c89972a908b55b663afb7fe46000b3312f8c
twyK <matteomartellini@outlook.com>
2018-11-14 16:27:49 +0100

    Impostazioni funzionanti

--- js/impostazioni.js
+++ js/impostazioni.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var refreshTime = localStorage.getItem("refreshTime");
+var refreshTime = localStorage.getItem("refreshTime") || 30000;
 
 window.onload = function () {
   // Imposta il tempo di aggiornamento della mappa in base alla scelta dell'utente
@@ -11,6 +11,21 @@ window.onload = function () {
     document.getElementById("refreshSetting").value = 30;
   }
 };
+
+function salvaRefreshTime () {
+  // Prende il valore numerico dall'input nella pagina
+  var temp = parseInt(document.getElementById("refreshSetting").value);
+
+  // Controllo del tempo di aggiornamento
+  if (temp >= 3 && temp <= 259200) {
+    // Salva il tempo di aggiornamento
+    refreshTime = temp;
+    localStorage.setItem("refreshTime", secToMs(refreshTime));
+
+    // Aggiorna la pagina per applicare le modifiche
+    window.location.reload();
+  }
+}
 
 function secToMs (sec) {
   return sec * 1000;
