e8e5f59a9552d9c28e41f6c3cd7eebdc71a3b461
twyK <matteomartellini@outlook.com>
2018-11-14 19:31:14 +0100

    Aggiunta impostazione simulazione + bottone salva globale

--- js/impostazioni.js
+++ js/impostazioni.js
@@ -3,6 +3,10 @@
 var refreshTime = localStorage.getItem("refreshTime") || 30000;
 
 window.onload = function () {
+  impostaValoriRefreshTime();
+};
+
+function impostaValoriRefreshTime () {
   // Imposta il tempo di aggiornamento della mappa in base alla scelta dell'utente
   if (refreshTime != null) {
     document.getElementById("refreshSetting").value = msToSec(refreshTime);
@@ -10,7 +14,7 @@ window.onload = function () {
     localStorage.setItem("refreshTime", 30000);
     document.getElementById("refreshSetting").value = 30;
   }
-};
+}
 
 function salvaRefreshTime () {
   // Prende il valore numerico dall'input nella pagina
@@ -21,10 +25,32 @@ function salvaRefreshTime () {
     // Salva il tempo di aggiornamento
     refreshTime = temp;
     localStorage.setItem("refreshTime", secToMs(refreshTime));
-
-    // Aggiorna la pagina per applicare le modifiche
-    window.location.reload();
   }
+}
+
+function impostaValoriSimulazione () {
+  if (localStorage.getItem("api") != null) {
+    document.getElementById("simulationBox").checked = true;
+  } else {
+    document.getElementById("simulationBox").checked = false;
+  }
+}
+
+function salvaSimulazione () {
+  // Imposta il server da cui prendere i dati
+  if (document.getElementById("simulationBox").checked === true) {
+    localStorage.setItem("api", "http://api.smarttrash.tk/");
+  } else {
+    localStorage.removeItem("api");
+  };
+}
+
+function salvaImpostazioni () {
+  salvaRefreshTime();
+  salvaSimulazione();
+
+  // Aggiorna la pagina per applicare le modifiche
+  window.location.reload();
 }
 
 function secToMs (sec) {
